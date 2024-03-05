b9022db5302eb56d9d9ac3cf088d49f0e37be4fc
Hojoon Kim <hojoon0724@gmail.com>
2024-03-05 14:25:37 -0800

    Initial Commit

---
+++ Xcode-firstApp/Xcode_firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_firstAppApp.swift
+//  Xcode-firstApp
+//
+//  Created by Hojoon Kim on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
